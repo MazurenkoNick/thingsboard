54cb414f585a1a7bf2c05723198d7d68b7c88a2f
Igor Kulikov <ikulikov@thingsboard.io>
2019-02-08 14:26:38 +0200

    Set Prototype version and limits.

--- dao/src/main/java/org/thingsboard/server/dao/subscription/BasicSubscriptionService.java
+++ dao/src/main/java/org/thingsboard/server/dao/subscription/BasicSubscriptionService.java
@@ -50,9 +50,9 @@ import org.thingsboard.server.dao.tenant.TenantService;
 @Slf4j
 public class BasicSubscriptionService implements SubscriptionService {
 
-    private static final long MAX_DEVICES = 10;
-    private static final long MAX_ASSETS = 10;
-    private static final boolean WHITE_LABELING_ENABLED = false;
+    private static final long MAX_DEVICES = 100;
+    private static final long MAX_ASSETS = 100;
+    private static final boolean WHITE_LABELING_ENABLED = true;
 
     @Autowired
     protected TenantService tenantService;
