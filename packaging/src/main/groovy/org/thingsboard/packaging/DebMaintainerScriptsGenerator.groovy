f7f0457b4eed5495266da6f748348ae21c37ec19
Igor Kulikov <ikulikov@thingsboard.io>
2019-06-18 15:11:07 +0300

    Add EULA to ThingsBoard debian packaging.

---
+++ packaging/src/main/groovy/org/thingsboard/packaging/DebMaintainerScriptsGenerator.groovy
@@ -0,0 +1,106 @@
+/**
+ * ThingsBoard, Inc. ("COMPANY") CONFIDENTIAL
+ *
+ * Copyright © 2016-2019 ThingsBoard, Inc. All Rights Reserved.
+ *
+ * NOTICE: All information contained herein is, and remains
+ * the property of ThingsBoard, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to ThingsBoard, Inc.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ *
+ * Dissemination of this information or reproduction of this material is strictly forbidden
+ * unless prior written permission is obtained from COMPANY.
+ *
+ * Access to the source code contained herein is hereby forbidden to anyone except current COMPANY employees,
+ * managers or contractors who have executed Confidentiality and Non-disclosure agreements
+ * explicitly covering such access.
+ *
+ * The copyright notice above does not evidence any actual or intended publication
+ * or disclosure  of  this source code, which includes
+ * information that is confidential and/or proprietary, and is a trade secret, of  COMPANY.
+ * ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC  PERFORMANCE,
+ * OR PUBLIC DISPLAY OF OR THROUGH USE  OF THIS  SOURCE CODE  WITHOUT
+ * THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED,
+ * AND IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES.
+ * THE RECEIPT OR POSSESSION OF THIS SOURCE CODE AND/OR RELATED INFORMATION
+ * DOES NOT CONVEY OR IMPLY ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS,
+ * OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
+ */
+package org.thingsboard.packaging
+
+import com.netflix.gradle.plugins.deb.TemplateHelper
+import com.netflix.gradle.plugins.utils.FileSystemActions
+import groovy.transform.Canonical
+
+class DebMaintainerScriptsGenerator {
+    private final DebPackaging task
+    private final TemplateHelper templateHelper
+    private final File destination
+    private final FileSystemActions fileSystem
+
+    DebMaintainerScriptsGenerator(DebPackaging task, TemplateHelper templateHelper, File destination, FileSystemActions fileSystem) {
+        this.destination = destination
+        this.task = task
+        this.templateHelper = templateHelper
+        this.fileSystem = fileSystem
+    }
+
+    void generate(Map<String, Object> context) {
+        templateHelper.generateFile("control", context)
+
+        def configurationFiles = task.allConfigurationFiles
+        if (configurationFiles.any()) {
+            templateHelper.generateFile("conffiles", [files: configurationFiles] )
+        }
+
+        def scripts = [
+                new MaintainerScript("preinst", task.preInstallFile, task.allPreInstallCommands),
+                new MaintainerScript("postinst", task.postInstallFile, task.allPostInstallCommands),
+                new MaintainerScript("prerm", task.preUninstallFile, task.allPreUninstallCommands),
+                new MaintainerScript("postrm", task.postUninstallFile, task.allPostUninstallCommands)
+        ]
+        if (task.configFile) {
+            scripts << new MaintainerScript("config", task.configFile, [])
+        }
+        if (task.templatesFile) {
+            scripts << new MaintainerScript("templates", task.templatesFile, [])
+        }
+
+        def installUtils = task.allCommonCommands.collect { stripShebang(it) }
+        for (script in scripts) {
+            if(script.file) {
+                fileSystem.copy(script.file, new File(destination, script.name))
+            } else {
+                templateHelper.generateFile(script.name, context + [commands: installUtils + script.commands.collect { stripShebang(it) }])
+            }
+        }
+    }
+
+    /**
+     * Works with nulls, Strings and Files.
+     *
+     * @param script
+     * @return
+     */
+    private static String stripShebang(Object script) {
+        StringBuilder result = new StringBuilder();
+        script?.eachLine { line ->
+            if (!line.matches('^#!.*$')) {
+                result.append line
+                result.append "\n"
+            }
+        }
+        result.toString()
+
+    }
+
+    @Canonical
+    private static class MaintainerScript {
+        String name
+        File file
+        List<Object> commands
+    }
+}
+
---
+++ packaging/src/main/groovy/org/thingsboard/packaging/DebPackagingCopyAction.groovy
@@ -0,0 +1,373 @@
+/**
+ * ThingsBoard, Inc. ("COMPANY") CONFIDENTIAL
+ *
+ * Copyright © 2016-2019 ThingsBoard, Inc. All Rights Reserved.
+ *
+ * NOTICE: All information contained herein is, and remains
+ * the property of ThingsBoard, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to ThingsBoard, Inc.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ *
+ * Dissemination of this information or reproduction of this material is strictly forbidden
+ * unless prior written permission is obtained from COMPANY.
+ *
+ * Access to the source code contained herein is hereby forbidden to anyone except current COMPANY employees,
+ * managers or contractors who have executed Confidentiality and Non-disclosure agreements
+ * explicitly covering such access.
+ *
+ * The copyright notice above does not evidence any actual or intended publication
+ * or disclosure  of  this source code, which includes
+ * information that is confidential and/or proprietary, and is a trade secret, of  COMPANY.
+ * ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC  PERFORMANCE,
+ * OR PUBLIC DISPLAY OF OR THROUGH USE  OF THIS  SOURCE CODE  WITHOUT
+ * THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED,
+ * AND IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES.
+ * THE RECEIPT OR POSSESSION OF THIS SOURCE CODE AND/OR RELATED INFORMATION
+ * DOES NOT CONVEY OR IMPLY ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS,
+ * OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
+ */
+package org.thingsboard.packaging
+
+import com.netflix.gradle.plugins.deb.control.MultiArch
+import com.netflix.gradle.plugins.deb.validation.DebTaskPropertiesValidator
+import com.netflix.gradle.plugins.deb.TemplateHelper
+import com.netflix.gradle.plugins.deb.DebFileVisitorStrategy
+import com.netflix.gradle.plugins.packaging.AbstractPackagingCopyAction
+import com.netflix.gradle.plugins.packaging.Dependency
+import com.netflix.gradle.plugins.packaging.Directory
+import com.netflix.gradle.plugins.packaging.Link
+import com.netflix.gradle.plugins.utils.ApacheCommonsFileSystemActions
+import groovy.transform.Canonical
+import org.apache.commons.lang3.StringUtils
+import org.apache.commons.lang3.time.DateFormatUtils
+import org.gradle.api.GradleException
+import org.gradle.api.internal.file.copy.CopyAction
+import org.gradle.api.internal.file.copy.FileCopyDetailsInternal
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.vafer.jdeb.Compression
+import org.vafer.jdeb.Console
+import org.vafer.jdeb.DataProducer
+import org.vafer.jdeb.DebMaker
+import org.vafer.jdeb.mapping.Mapper
+import org.vafer.jdeb.mapping.PermMapper
+import org.vafer.jdeb.producers.DataProducerLink
+import org.vafer.jdeb.producers.DataProducerPathTemplate
+
+import static com.netflix.gradle.plugins.utils.GradleUtils.lookup
+/**
+ * Forked and modified from org.jamel.pkg4j.gradle.tasks.BuildDebTask
+ */
+class DebPackagingCopyAction extends AbstractPackagingCopyAction<DebPackaging> {
+    static final Logger logger = LoggerFactory.getLogger(DebPackagingCopyAction.class)
+
+    File debianDir
+    List<String> dependencies
+    List<String> conflicts
+    List<String> recommends
+    List<String> suggests
+    List<String> enhances
+    List<String> preDepends
+    List<String> breaks
+    List<String> replaces
+    List<String> provides
+    List<DataProducer> dataProducers
+    List<InstallDir> installDirs
+    private final DebTaskPropertiesValidator debTaskPropertiesValidator = new DebTaskPropertiesValidator()
+    private DebFileVisitorStrategy debFileVisitorStrategy
+    private final DebMaintainerScriptsGenerator maintainerScriptsGenerator
+
+    DebPackagingCopyAction(DebPackaging debTask) {
+        super(debTask)
+        debTaskPropertiesValidator.validate(debTask)
+        dependencies = []
+        conflicts = []
+        recommends = []
+        suggests = []
+        enhances = []
+        preDepends = []
+        breaks = []
+        replaces = []
+        dataProducers = []
+        installDirs = []
+        provides = []
+        debianDir = new File(task.project.buildDir, "debian")
+        debFileVisitorStrategy = new DebFileVisitorStrategy(dataProducers, installDirs)
+        maintainerScriptsGenerator = new DebMaintainerScriptsGenerator(debTask, new TemplateHelper(debianDir, '/deb'), debianDir, new ApacheCommonsFileSystemActions())
+    }
+
+    @Canonical
+    static class InstallDir {
+        String name
+        String user
+        String group
+    }
+
+    @Override
+    void startVisit(CopyAction action) {
+        super.startVisit(action)
+
+        debianDir.deleteDir()
+        debianDir.mkdirs()
+
+    }
+
+    @Override
+    void visitFile(FileCopyDetailsInternal fileDetails, def specToLookAt) {
+        logger.debug "adding file {}", fileDetails.relativePath.pathString
+
+        def inputFile = extractFile(fileDetails)
+
+        String user = lookup(specToLookAt, 'user') ?: task.user
+        Integer uid = (Integer) lookup(specToLookAt, 'uid') ?: task.uid ?: 0
+        String group = lookup(specToLookAt, 'permissionGroup') ?: task.permissionGroup
+        Integer gid = (Integer) lookup(specToLookAt, 'gid') ?: task.gid ?: 0
+
+        int fileMode = fileDetails.mode
+
+        debFileVisitorStrategy.addFile(fileDetails, inputFile, user, uid, group, gid, fileMode)
+    }
+
+    @Override
+    void visitDir(FileCopyDetailsInternal dirDetails, def specToLookAt) {
+        def specCreateDirectoryEntry = lookup(specToLookAt, 'createDirectoryEntry')
+        boolean createDirectoryEntry = specCreateDirectoryEntry!=null ? specCreateDirectoryEntry : task.createDirectoryEntry
+        if (createDirectoryEntry) {
+
+            logger.debug "adding directory {}", dirDetails.relativePath.pathString
+            String user = lookup(specToLookAt, 'user') ?: task.user
+            Integer uid = (Integer) lookup(specToLookAt, 'uid') ?: task.uid ?: 0
+            String group = lookup(specToLookAt, 'permissionGroup') ?: task.permissionGroup
+            Integer gid = (Integer) lookup(specToLookAt, 'gid') ?: task.gid ?: 0
+
+            int fileMode = dirDetails.mode
+
+            debFileVisitorStrategy.addDirectory(dirDetails, user, uid, group, gid, fileMode)
+        }
+    }
+
+    @Override
+    protected void addLink(Link link) {
+        dataProducers << new DataProducerLink(link.path, link.target, true, null, null, null);
+    }
+
+    @Override
+    protected void addDependency(Dependency dep) {
+        dependencies << dep.toDebString()
+    }
+
+    @Override
+    protected void addConflict(Dependency dep) {
+        conflicts << dep.toDebString()
+    }
+
+    @Override
+    protected void addProvides(Dependency dep) {
+        provides << dep.packageName
+    }
+
+    @Override
+    protected void addObsolete(Dependency dep) {
+        logger.warn "Obsoletes functionality not implemented for deb files"
+    }
+
+    protected void addRecommends(Dependency dep) {
+        recommends << dep.toDebString()
+    }
+
+    protected void addSuggests(Dependency dep) {
+        suggests << dep.toDebString()
+    }
+
+    protected void addEnhances(Dependency dep) {
+        enhances << dep.toDebString()
+    }
+
+    protected void addPreDepends(Dependency dep) {
+        preDepends << dep.toDebString()
+    }
+
+    protected void addBreaks(Dependency dep) {
+        breaks << dep.toDebString()
+    }
+
+    protected void addReplaces(Dependency dep) {
+        replaces << dep.toDebString()
+    }
+
+    @Override
+    protected void addDirectory(Directory directory) {
+        dataProducers << new DataProducerPathTemplate(
+                [directory.path] as String[], null, null,
+                [ new PermMapper(-1, -1, task.user, task.permissionGroup,
+                        directory.permissions, -1, 0, null) ] as Mapper[])
+    }
+
+    protected String getMultiArch() {
+        def archString = task.getArchString()
+        def multiArch = task.getMultiArch()
+        if (('all' == archString) && (MultiArch.SAME == multiArch)) {
+            throw new IllegalArgumentException('Deb packages with Architecture: all cannot declare Multi-Arch: same')
+        }
+        def multiArchString = multiArch?.name()?.toLowerCase() ?: ''
+        return multiArchString
+    }
+
+    protected Map<String,String> getCustomFields() {
+        task.getAllCustomFields().collectEntries { String key, String val ->
+            // in the deb control file, header XB-Foo becomes Foo in the binary package
+            ['XB-' + key.capitalize(), val]
+        }
+    }
+
+    @Override
+    protected void end() {
+        for (Dependency recommends : task.allRecommends) {
+            logger.debug "adding recommends on {} {}", recommends.packageName, recommends.version
+            addRecommends(recommends)
+        }
+
+        for (Dependency suggests : task.allSuggests) {
+            logger.debug "adding suggests on {} {}", suggests.packageName, suggests.version
+            addSuggests(suggests)
+        }
+
+        for (Dependency enhances : task.allEnhances) {
+            logger.debug "adding enhances on {} {}", enhances.packageName, enhances.version
+            addEnhances(enhances)
+        }
+
+        for (Dependency preDepends : task.allPreDepends) {
+            logger.debug "adding preDepends on {} {}", preDepends.packageName, preDepends.version
+            addPreDepends(preDepends)
+        }
+
+        for (Dependency breaks : task.getAllBreaks()) {
+            logger.debug "adding breaks on {} {}", breaks.packageName, breaks.version
+            addBreaks(breaks)
+        }
+
+        for (Dependency replaces : task.allReplaces) {
+            logger.debug "adding replaces on {} {}", replaces.packageName, replaces.version
+            addReplaces(replaces)
+        }
+
+        maintainerScriptsGenerator.generate(toContext())
+
+        task.allSupplementaryControlFiles.each { supControl ->
+            File supControlFile = supControl instanceof File ? supControl : task.project.file(supControl)
+            new File(debianDir, supControlFile.name).bytes = supControlFile.bytes
+        }
+
+        DebMaker maker = new DebMaker(new GradleLoggerConsole(), dataProducers, null)
+        File debFile = task.getArchivePath()
+        maker.setControl(debianDir)
+        maker.setDeb(debFile)
+        if (StringUtils.isNotBlank(task.getSigningKeyId())
+                && StringUtils.isNotBlank(task.getSigningKeyPassphrase())
+                && task.getSigningKeyRingFile().exists()) {
+            maker.setKey(task.getSigningKeyId())
+            maker.setPassphrase(task.getSigningKeyPassphrase())
+            maker.setKeyring(task.getSigningKeyRingFile())
+            maker.setSignPackage(true)
+        }
+
+        logger.info("Creating debian package: ${debFile}")
+
+        try {
+            logger.info("Creating debian package: ${debFile}")
+            maker.setCompression(Compression.GZIP.toString())
+            maker.makeDeb()
+        } catch (Exception e) {
+            throw new GradleException("Can't build debian package ${debFile}", e)
+        }
+
+        // TODO Put changes file into a separate task
+        //def changesFile = new File("${packagePath}_all.changes")
+        //createChanges(pkg, changesFile, descriptor, processor)
+
+        logger.info 'Created deb {}', debFile
+    }
+
+    private static class GradleLoggerConsole implements Console {
+        @Override
+        void debug(String message) {
+            logger.debug(message)
+        }
+
+        @Override
+        void info(String message) {
+            logger.info(message)
+        }
+
+        @Override
+        void warn(String message) {
+            logger.warn(message)
+        }
+    }
+
+    /**
+     * Map to be consumed by generateFile when transforming template
+     */
+    def Map toContext() {
+        [
+                name: task.getPackageName(),
+                version: task.getVersion(),
+                release: task.getRelease(),
+                maintainer: task.getMaintainer(),
+                uploaders: task.getUploaders(),
+                priority: task.getPriority(),
+                epoch: task.getEpoch(),
+                description: task.getPackageDescription() ?: '',
+                distribution: task.getDistribution(),
+                summary: task.getSummary(),
+                section: task.getPackageGroup(),
+                time: DateFormatUtils.SMTP_DATETIME_FORMAT.format(new Date()),
+                provides: StringUtils.join(provides, ", "),
+                depends: StringUtils.join(dependencies, ", "),
+                url: task.getUrl(),
+                arch: task.getArchString(),
+                multiArch: getMultiArch(),
+                conflicts: StringUtils.join(conflicts, ", "),
+                recommends: StringUtils.join(recommends, ", "),
+                suggests: StringUtils.join(suggests, ", "),
+                enhances: StringUtils.join(enhances, ", " ),
+                preDepends: StringUtils.join(preDepends, ", "),
+                breaks: StringUtils.join(breaks, ", "),
+                replaces: StringUtils.join(replaces, ", "),
+                fullVersion: buildFullVersion(),
+                customFields: getCustomFields(),
+
+                // Uses install command for directory
+                dirs: installDirs.collect { InstallDir dir ->
+                    def map = [name: dir.name]
+                    if(dir.user) {
+                        if (dir.group) {
+                            map['owner'] = "${dir.user}:${dir.group}"
+                        } else {
+                            map['owner'] = dir.user
+                        }
+                    }
+                    return map
+                }
+        ]
+    }
+
+    private String buildFullVersion() {
+        StringBuilder fullVersion = new StringBuilder()
+        if (task.getEpoch() != 0) {
+            fullVersion <<= task.getEpoch()
+            fullVersion <<= ':'
+        }
+        fullVersion <<= task.getVersion()
+
+        if(task.getRelease()) {
+            fullVersion <<= '-'
+            fullVersion <<= task.getRelease()
+        }
+
+        fullVersion.toString()
+    }
+}
