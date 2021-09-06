efad5116a1c6bcc34ad8a5301046b986a2b72950
Igor Kulikov <ikulikov@thingsboard.io>
2021-09-06 14:44:58 +0300

    Fix subscription violation processing by http interceptor

--- ui-ngx/src/app/core/interceptors/global-http-interceptor.ts
+++ ui-ngx/src/app/core/interceptors/global-http-interceptor.ts
@@ -126,9 +126,7 @@ export class GlobalHttpInterceptor implements HttpInterceptor {
       }
     } else if (errorCode && errorCode === Constants.serverErrorCode.subscriptionViolation) {
       if (!ignoreErrors) {
-        return this.dialogService.subscriptionViolation(errorResponse.error).pipe(
-          mergeMap(() => throwError(errorResponse))
-        );
+        this.dialogService.subscriptionViolation(errorResponse.error);
       }
     } else if (errorResponse.status === 429) {
       if (resendRequest) {
