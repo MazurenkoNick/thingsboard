94c7be36f7fd0e306e9311825977d46fa77bb7d2
Igor Kulikov <ikulikov@thingsboard.io>
2025-04-17 12:39:56 +0300

    Add Report Table

--- dao/src/main/resources/sql/schema-entities.sql
+++ dao/src/main/resources/sql/schema-entities.sql
@@ -676,6 +676,21 @@ CREATE TABLE IF NOT EXISTS blob_entity (
     additional_info varchar
 ) PARTITION BY RANGE (created_time);
 
+CREATE TABLE IF NOT EXISTS report (
+    id uuid NOT NULL CONSTRAINT report_pkey PRIMARY KEY,
+    created_time bigint NOT NULL,
+    tenant_id uuid,
+    customer_id uuid,
+    name varchar(255),
+    description varchar(1024),
+    configuration varchar(10000000),
+    scheduler_event_id uuid UNIQUE,
+    external_id uuid,
+    version BIGINT DEFAULT 1,
+    CONSTRAINT report_external_id_unq_key UNIQUE (tenant_id, external_id),
+    CONSTRAINT fk_report_scheduler_event FOREIGN KEY (scheduler_event_id) REFERENCES scheduler_event(id) ON DELETE SET NULL
+);
+
 CREATE TABLE IF NOT EXISTS entity_view (
     id uuid NOT NULL CONSTRAINT entity_view_pkey PRIMARY KEY,
     created_time bigint NOT NULL,
