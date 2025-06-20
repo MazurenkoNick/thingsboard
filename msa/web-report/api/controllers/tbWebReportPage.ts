047c48a6a9914cb4c867ac6986a02fc191b3e929
Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
2025-06-20 11:53:39 +0300

    UI: Fixed missing web report generation for widgets with lazy-loaded content

--- msa/web-report/api/controllers/tbWebReportPage.ts
+++ msa/web-report/api/controllers/tbWebReportPage.ts
@@ -40,24 +40,13 @@ import {
 import { performance } from 'perf_hooks';
 import { _logger } from '../../config/logger';
 import config from 'config';
-import { GenerateReportRequest, OpenReportMessage, ReportResultMessage } from './tbWebReportModels';
+import { GenerateReportRequest, OpenReportMessage, ReportResultMessage, WaitWidgetsMessage } from './tbWebReportModels';
 import winston from 'winston';
 
 const defaultPageNavigationTimeout = Number(config.get('browser.defaultPageNavigationTimeout'));
 const loadDashboardResourcesTimeout = Number(config.get('browser.loadDashboardResourcesTimeout'));
 const dashboardIdleWaitTime = Number(config.get('browser.dashboardIdleWaitTime'));
 const useNewPage = Boolean(config.get('browser.useNewPage'));
-
-const heightCalculationScript = "var height = 0;\n" +
-    "     var gridsterChild = document.getElementById('gridster-child');\n" +
-    "     if (gridsterChild) {\n" +
-    "         height = Number(document.getElementById('gridster-child').scrollHeight);\n" +
-    "         var dashboardTitleElements = document.getElementsByClassName(\"tb-dashboard-title\");\n" +
-    "         if (dashboardTitleElements && dashboardTitleElements.length) {\n" +
-    "              height += Number(dashboardTitleElements[0].offsetHeight);\n" +
-    "         }\n" +
-    "     }\n" +
-    "     Math.round(height);";
 
 export class TbWebReportPage {
 
@@ -182,11 +171,6 @@ export class TbWebReportPage {
             if (dashboardIdleWaitTime > 0) {
                 await this.page.waitForTimeout(dashboardIdleWaitTime);
             }
-            const fullHeight: number = await this.page.evaluate(heightCalculationScript);
-
-            const newHeight = fullHeight || 1080;
-
-            await this.setPageHeight(newHeight);
 
             if (request.type === 'pdf') {
                 buffer = await this.page.pdf({printBackground: true, width: '1920px', height: this.pageHeight + 'px'});
@@ -225,6 +209,18 @@ export class TbWebReportPage {
         const result = await this.waitForReportResult('open report', loadDashboardResourcesTimeout * 3);
         if (!result.success) {
             throw new Error(result.error);
+        }
+
+        const newHeight = result.pageHeight ?? 1080;
+        await this.setPageHeight(newHeight);
+
+        const waitWidgetsMessage: WaitWidgetsMessage = {
+            timeout: loadDashboardResourcesTimeout
+        }
+        await this.postWindowMessage({type: 'waitReportWidgets', data: waitWidgetsMessage});
+        const waitWidgetsResult = await this.waitForReportResult('wait widgets', loadDashboardResourcesTimeout * 2);
+        if (!waitWidgetsResult.success) {
+            throw new Error(waitWidgetsResult.error);
         }
     }
 
