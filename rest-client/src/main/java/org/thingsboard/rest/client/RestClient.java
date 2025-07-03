fc17ba9f2419fd1f5c3e8f667f47f672fb4af88c
yevhenii <uman980124@gmail.com>
2025-07-03 15:34:30 +0300

    Implement white label remove methods

--- rest-client/src/main/java/org/thingsboard/rest/client/RestClient.java
+++ rest-client/src/main/java/org/thingsboard/rest/client/RestClient.java
@@ -5050,12 +5050,16 @@ public class RestClient implements Closeable {
                 WhiteLabelingParams.class).getBody();
     }
 
+    public void deleteWhiteLabelParams() {restTemplate.delete(baseURL + "/api/whiteLabel/currentWhiteLabelParams");}
+
     public LoginWhiteLabelingParams saveLoginWhiteLabelParams(LoginWhiteLabelingParams loginWhiteLabelingParams) {
         return restTemplate.postForEntity(
                 baseURL + "/api/whiteLabel/loginWhiteLabelParams",
                 loginWhiteLabelingParams,
                 LoginWhiteLabelingParams.class).getBody();
     }
+
+    public void deleteLoginWhiteLabelParams() {restTemplate.delete(baseURL + "/api/whiteLabel/currentLoginWhiteLabelParams");}
 
     public WhiteLabelingParams previewWhiteLabelParams(WhiteLabelingParams whiteLabelingParams) {
         return restTemplate.postForEntity(
@@ -5085,4 +5089,5 @@ public class RestClient implements Closeable {
                 baseURL + "/api/whiteLabel/appThemeCss",
                 paletteSettings, String.class).getBody();
     }
+
 }
