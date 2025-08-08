a4e36e49b705f69905942b61dea17a189c3d60ab
deaflynx <artemio.babak@gmail.com>
2025-08-08 17:06:02 +0300

    Feature custom image icon fixes after review.

--- ui-ngx/src/app/shared/components/material-icon-select.component.ts
+++ ui-ngx/src/app/shared/components/material-icon-select.component.ts
@@ -51,7 +51,6 @@ import { TbPopoverService } from '@shared/components/popover.service';
 import { MaterialIconsComponent } from '@shared/components/material-icons.component';
 import { MatButton } from '@angular/material/button';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { isTbImage } from '@shared/models/resource.models';
 
 @Component({
   selector: 'tb-material-icon-select',
@@ -90,8 +89,6 @@ export class MaterialIconSelectComponent extends PageComponent implements OnInit
   @Input()
   @coerceBoolean()
   allowedCustomIcon = false;
-
-  isCustomIcon = false;
 
   private requiredValue: boolean;
   get required(): boolean {
@@ -153,13 +150,11 @@ export class MaterialIconSelectComponent extends PageComponent implements OnInit
     this.materialIconFormGroup.patchValue(
       { icon: this.modelValue }, {emitEvent: false}
     );
-    this.defineIconType(value);
   }
 
   private updateModel() {
     const icon: string = this.materialIconFormGroup.get('icon').value;
     if (this.modelValue !== icon) {
-      this.defineIconType(icon);
       this.modelValue = icon;
       this.propagateChange(this.modelValue);
     }
@@ -212,11 +207,5 @@ export class MaterialIconSelectComponent extends PageComponent implements OnInit
   clear() {
     this.materialIconFormGroup.get('icon').patchValue(null, {emitEvent: true});
     this.cd.markForCheck();
-  }
-
-  private defineIconType(icon: string) {
-    if (this.allowedCustomIcon) {
-      this.isCustomIcon = isTbImage(icon);
-    }
   }
 }
