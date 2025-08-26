2402d16e7821b35e8107be44dc3004c3d4719165
Igor Kulikov <ikulikov@thingsboard.io>
2025-08-26 20:19:53 +0300

    Time series renderer - improve y axes ticks detection.

--- common/report/src/main/java/org/thingsboard/server/report/renderer/chart/TbNumberAxis.java
+++ common/report/src/main/java/org/thingsboard/server/report/renderer/chart/TbNumberAxis.java
@@ -30,28 +30,48 @@
  */
 package org.thingsboard.server.report.renderer.chart;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.NumberTick;
 import org.jfree.chart.axis.NumberTickUnit;
+import org.jfree.chart.axis.Tick;
+import org.jfree.chart.axis.TickUnit;
+import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.ui.RectangleEdge;
+import org.jfree.chart.ui.TextAnchor;
 import org.jfree.data.Range;
 
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TbNumberAxis extends NumberAxis {
 
+    @Setter
+    private Double axisMin;
+    @Setter
+    private Double axisMax;
+    @Setter
     private Integer splitNumber;
+    private TbNumberTickUnitSource tbNumberTickUnitSource = new TbNumberTickUnitSource();
+    private TbNumberAxisTicks ticks = new TbNumberAxisTicks();
+
+    private TbNumberAxis parentAxis = null;
 
     public TbNumberAxis() {
-        super();
+        this(null, null);
     }
 
     public TbNumberAxis(String label) {
-        super(label);
+        this(label, null);
     }
 
-    public void setSplitNumber(Integer splitNumber) {
-        this.splitNumber = splitNumber;
+    public TbNumberAxis(String label, TbNumberAxis parentAxis) {
+        super(label);
+        this.parentAxis = parentAxis;
     }
 
     @Override
@@ -64,38 +84,207 @@ public class TbNumberAxis extends NumberAxis {
     @Override
     protected void autoAdjustRange() {
         super.autoAdjustRange();
-        double lower = getRange().getLowerBound();
-        double upper = getRange().getUpperBound();
-        if (upper > lower) {
-            NumberTickUnit tickUnit = getTickUnit();
-            if (splitNumber != null) {
-                double size = getRange().getLength() / (splitNumber + 1);
-                tickUnit = (NumberTickUnit) this.getStandardTickUnits().getCeilingTickUnit(size);
-                setTickUnit(tickUnit);
-            }
+        double lower = this.axisMin != null ? this.axisMin : getRange().getLowerBound();
+        double upper = this.axisMax != null ? this.axisMax : getRange().getUpperBound();
+        if (upper <= lower) {
+            upper = lower + 1.0;
+        }
+        double length = upper - lower;
+        NumberTickUnit tickUnit = getTickUnit();
+        double size = tickUnit.getSize();
+        if (splitNumber != null) {
+            size = length / splitNumber;
+        }
+        if (isAutoTickUnitSelection()) {
+            tickUnit = (NumberTickUnit) this.tbNumberTickUnitSource.getCeilingTickUnit(size);
+            setTickUnit(tickUnit, false, false);
+        }
+
+        double currentAxisMin;
+        double currentAxisMax;
+        lower = getRange().getLowerBound();
+        upper = getRange().getUpperBound();
+        if (this.axisMin == null) {
             double rest = lower % tickUnit.getSize();
             if (rest > 0) {
                 lower -= rest;
             }
-            rest = (upper - lower) % tickUnit.getSize();
+            currentAxisMin = lower;
+        } else {
+            currentAxisMin = this.axisMin;
+        }
+        if (this.axisMax == null) {
+            double rest = (upper - lower) % tickUnit.getSize();
             if (rest > 0) {
                 double upperAdjust = tickUnit.getSize() - rest;
                 upper += upperAdjust;
             }
-            setRange(new Range(lower, upper), false, false);
+            currentAxisMax = upper;
+        } else {
+            currentAxisMax = this.axisMax;
         }
+        if (currentAxisMax <= currentAxisMin) {
+            currentAxisMax = currentAxisMin + 1.0;
+        }
+        setRange(new Range(currentAxisMin, currentAxisMax), false, false);
+        this.calculateTicks();
     }
 
     @Override
-    protected int calculateVisibleTickCount() {
-        //if (splitNumber != null) {
-        //    return splitNumber + 1;
-        //} else {
-            double unit = getTickUnit().getSize();
-            Range range = getRange();
-            return (int) (Math.floor(range.getUpperBound() / unit)
-                    - Math.ceil(range.getLowerBound() / unit) + 1);
-        //}
+    protected List<Tick> refreshTicksVertical(Graphics2D g2,
+                                              Rectangle2D dataArea, RectangleEdge edge) {
+
+        List<Tick> result = new ArrayList<>();
+        Font tickLabelFont = getTickLabelFont();
+        g2.setFont(tickLabelFont);
+        if (isAutoTickUnitSelection()) {
+            selectAutoTickUnit(g2, dataArea, edge);
+        }
+
+        if (ticks.getTicksCount() <= ValueAxis.MAXIMUM_TICK_COUNT) {
+
+            boolean drawFirstLabel = true;
+            boolean drawLastLabel = true;
+
+            TbNumberAxisTicks ticks = getTicks();
+            if (ticks.checkFirstTickIntersection()) {
+                double labelHeight = estimateMaximumTickLabelHeight(g2);
+                double firstLabelPos = valueToJava2D(ticks.firstTickValue(getRange()), dataArea, edge);
+                double nextLabelPos = valueToJava2D(ticks.getTickValue(1, getRange()), dataArea, edge);
+                if ((firstLabelPos - labelHeight / 2) < (nextLabelPos + labelHeight / 2)) {
+                    drawFirstLabel = false;
+                }
+            }
+
+            if (ticks.checkLastTickIntersection()) {
+                double labelHeight = estimateMaximumTickLabelHeight(g2);
+                double lastLabelPos = valueToJava2D(ticks.lastTickValue(getRange()), dataArea, edge);
+                double prevLabelPos = valueToJava2D(ticks.getTickValue(ticks.getTicksCount()-2, getRange()), dataArea, edge);
+                if ((lastLabelPos + labelHeight / 2) > (prevLabelPos - labelHeight / 2)) {
+                    drawLastLabel = false;
+                }
+            }
+
+            for (int i = 0; i < ticks.getTicksCount(); i++) {
+                double currentTickValue = ticks.getTickValue(i, getRange());
+                if (currentTickValue == 0.0 && Double.compare(currentTickValue, 0.0) < 0) {
+                    currentTickValue = 0.0;
+                }
+                String tickLabel = null;
+                boolean drawLabel = (i == 0 && drawFirstLabel) || (i == ticks.getTicksCount() - 1 && drawLastLabel) || i > 0 && i < ticks.getTicksCount() - 1;
+                if (drawLabel) {
+                    NumberFormat formatter = getNumberFormatOverride();
+                    if (formatter != null) {
+                        tickLabel = formatter.format(currentTickValue);
+                    } else {
+                        tickLabel = getTickUnit().valueToString(currentTickValue);
+                    }
+                }
+
+                TextAnchor anchor;
+                TextAnchor rotationAnchor;
+                double angle = 0.0;
+                if (edge == RectangleEdge.LEFT) {
+                    anchor = TextAnchor.CENTER_RIGHT;
+                    rotationAnchor = TextAnchor.CENTER_RIGHT;
+                }
+                else {
+                    anchor = TextAnchor.CENTER_LEFT;
+                    rotationAnchor = TextAnchor.CENTER_LEFT;
+                }
+                Tick tick = new NumberTick(currentTickValue, tickLabel, anchor,
+                        rotationAnchor, angle);
+                result.add(tick);
+            }
+        }
+        return result;
     }
+
+    private TbNumberAxisTicks getTicks() {
+        if (this.parentAxis != null) {
+            return this.parentAxis.getTicks();
+        } else {
+            return this.ticks;
+        }
+    }
+
+    private void calculateTicks() {
+        if (this.parentAxis == null) {
+            TickUnit tu = getTickUnit();
+            double size = tu.getSize();
+            this.ticks.clear();
+            double currentTickValue = getRange().getLowerBound();
+            double maxTickValue = getRange().getUpperBound();
+            this.ticks.addTickValue(currentTickValue, getRange());
+            double lowestVisibleTickValue = isAutoTickUnitSelection() ? calculateLowestVisibleTickValue() : currentTickValue;
+            if (lowestVisibleTickValue > currentTickValue) {
+                this.ticks.setAdditionalFistTick(true);
+                currentTickValue = lowestVisibleTickValue;
+            } else {
+                currentTickValue += size;
+            }
+            while (currentTickValue < maxTickValue) {
+                this.ticks.addTickValue(currentTickValue, getRange());
+                currentTickValue += size;
+            }
+            if (maxTickValue > getRange().getLowerBound()) {
+                if (maxTickValue != currentTickValue) {
+                    this.ticks.setAdditionalLastTick(true);
+                }
+                this.ticks.addTickValue(maxTickValue, getRange());
+            }
+        }
+    }
+
+    private static class TbNumberAxisTicks {
+
+        private final List<Double> tickValues = new ArrayList<>();
+
+        @Getter
+        @Setter
+        private boolean additionalFistTick = false;
+
+        @Getter
+        @Setter
+        private boolean additionalLastTick = false;
+
+        public TbNumberAxisTicks() {}
+
+        public void addTickValue(double tickValue, Range range) {
+            tickValues.add((tickValue - range.getLowerBound()) / range.getLength());
+        }
+
+        public int getTicksCount() {
+            return tickValues.size();
+        }
+
+        public Double getTickValue(int index, Range range) {
+            return range.getLowerBound()  + tickValues.get(index) * range.getLength();
+        }
+
+        public Double firstTickValue(Range range) {
+            return getTickValue(0, range);
+        }
+
+        public Double lastTickValue(Range range) {
+            return getTickValue(tickValues.size() - 1, range);
+        }
+
+        public void clear() {
+            this.tickValues.clear();
+            this.additionalFistTick = false;
+            this.additionalLastTick = false;
+        }
+
+        public boolean checkFirstTickIntersection() {
+            return this.additionalFistTick && this.tickValues.size() > 2;
+        }
+
+        public boolean checkLastTickIntersection() {
+            return this.additionalLastTick && this.tickValues.size() > 2;
+        }
+
+    }
+
 
 }
