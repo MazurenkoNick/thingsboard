fbe455779522f1c42a35ec07efa2133ff9ba2c23
Maksym Tsymbarov <mtsymbarov@thingsboard.io>
2025-09-25 09:24:05 +0300

    optimize logic

--- ui-ngx/src/app/modules/home/components/dashboard-page/dashboard-page.component.ts
+++ ui-ngx/src/app/modules/home/components/dashboard-page/dashboard-page.component.ts
@@ -234,13 +234,7 @@ export class DashboardPageComponent extends PageComponent implements IDashboardC
   }
 
   get hideToolbar(): boolean {
-    const showToolbarInSpecialCase = this.forceFullscreen && this.hideToolbarSetting() && !this.isMobile;
-  
-    if (showToolbarInSpecialCase) {
-      return this.isEditingWidget || this.isAddingWidget;
-    }
-    
-    return ((this.hideToolbarValue || this.hideToolbarSetting()) && !this.isEdit) || (this.isEditingWidget || this.isAddingWidget) || !(this.isMobile || !this.forceFullscreen);
+    return ((this.hideToolbarValue || this.hideToolbarSetting()) && !this.isEdit) || (this.isEditingWidget || this.isAddingWidget);
   }
 
   @Input()
@@ -729,9 +723,9 @@ export class DashboardPageComponent extends PageComponent implements IDashboardC
   }
 
   private hideToolbarSetting(): boolean {
-    if (this.dashboard.configuration.settings &&
-      isDefined(this.dashboard.configuration.settings.hideToolbar)) {
-      return this.dashboard.configuration.settings.hideToolbar;
+    if (isDefined(this.dashboard.configuration?.settings?.hideToolbar)) {
+      const check = !this.forceFullscreen || this.isMobileApp;
+      return this.dashboard.configuration.settings.hideToolbar && check;
     } else {
       return false;
     }
