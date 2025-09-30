7419ddb51c384841267f0a02670a75e48d3e54ab
Maksym Tsymbarov <mtsymbarov@thingsboard.io>
2025-09-30 14:48:13 +0300

    moved to email regex validation

--- ui-ngx/src/app/core/utils.ts
+++ ui-ngx/src/app/core/utils.ts
@@ -52,6 +52,7 @@ import { SecurityContext } from '@angular/core';
 import { AbstractControl, ValidationErrors, Validators } from '@angular/forms';
 
 const varsRegex = /\${([^}]*)}/g;
+const emailRegex = /^[A-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
 export function onParentScrollOrWindowResize(el: Node): Observable<Event> {
   const scrollSubject = new Subject<Event>();
@@ -1120,12 +1121,6 @@ export function debounce<T extends (...args: any[]) => any>(
 }
 
 export const validateEmail = (control: AbstractControl): ValidationErrors | null => {
-  const email = control.value;
-  const nativeEmailError = Validators.email(control);
-  if (nativeEmailError !== null) {
-    return nativeEmailError;
-  }
-  const passesDomainCheck = /\.[^.\s]{2,}$/.test(email);
-  return passesDomainCheck ? null : {email: true};
+  return emailRegex.test(control.value) ? null : {email: true};
 };
 
