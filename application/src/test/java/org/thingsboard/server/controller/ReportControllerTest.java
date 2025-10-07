a4f152ce34b1235804dbf39a2183c03daef6a5d5
dashevchenko <dshevchenko@thingsboard.io>
2025-10-07 12:09:53 +0300

    added logging for flaky test

--- application/src/test/java/org/thingsboard/server/controller/ReportControllerTest.java
+++ application/src/test/java/org/thingsboard/server/controller/ReportControllerTest.java
@@ -197,7 +197,9 @@ public class ReportControllerTest extends AbstractControllerTest {
 
         await().atMost(60, TimeUnit.SECONDS).until(() -> {
             String csvReport = doPost("/api/v2/report/test", request, String.class);
-            return Arrays.stream(csvReport.split("\\r?\\n")).map(String::trim).toList()
+            List<String> actualReportRows = Arrays.stream(csvReport.split("\\r?\\n")).map(String::trim).toList();
+            log.debug("Report rows: {}", actualReportRows);
+            return actualReportRows
                     .containsAll(List.of("CREATED TIME,NAME,TEMPERATURE", formatter.format(Instant.ofEpochMilli(finalTestDevice.getCreatedTime())) + "," + finalTestDevice.getName() + ",5"));
         });
     }
