d0ea57d7f59d4974ac042c31b7516b38c72c3cf1
Andrii Landiak <andriylandiak@gmail.com>
2025-11-11 17:11:50 +0200

    Use customerStandaloneEntityPermissionChecker for customerStandaloneEntityPermissionChecker

--- application/src/main/java/org/thingsboard/server/service/security/permission/CustomerUserPermissions.java
+++ application/src/main/java/org/thingsboard/server/service/security/permission/CustomerUserPermissions.java
@@ -116,7 +116,7 @@ public class CustomerUserPermissions extends AbstractPermissions {
         put(Resource.DOMAIN, customerStandaloneEntityPermissionChecker);
         put(Resource.REPORT_TEMPLATE, reportTemplatePermissionChecker);
         put(Resource.REPORT, customerStandaloneEntityPermissionChecker);
-        put(Resource.API_KEY, genericPermissionChecker);
+        put(Resource.API_KEY, customerStandaloneEntityPermissionChecker);
     }
 
     private final PermissionChecker<AlarmId, Alarm> customerAlarmPermissionChecker = new PermissionChecker<>() {
