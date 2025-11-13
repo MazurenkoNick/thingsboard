9442269263f170a787b2454adf0566fb6aaa6adf
Andrii Landiak <andriylandiak@gmail.com>
2025-11-13 15:52:40 +0200

    Introduce SystemPatchApplier to update system data when patch version increased on startup

--- application/src/test/java/org/thingsboard/server/system/BaseRestApiLimitsTest.java
+++ application/src/test/java/org/thingsboard/server/system/BaseRestApiLimitsTest.java
@@ -60,10 +60,6 @@ import java.util.concurrent.TimeoutException;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-/**
- * @author Illia Barkov
- */
-
 @Slf4j
 public abstract class BaseRestApiLimitsTest extends AbstractControllerTest {
 
