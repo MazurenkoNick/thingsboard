656482d5f319221d0e0e91f05a8dc551762e8dc5
ArtemDzhereleiko <adzhereleiko@thingsboard.io>
2025-11-19 10:55:40 +0200

    UI: Add permission check on menu item

--- ui-ngx/src/app/core/services/menu.models.ts
+++ ui-ngx/src/app/core/services/menu.models.ts
@@ -1292,6 +1292,10 @@ const menuFilters = new Map<MenuId, MenuFilter>([
           userPermissionsService.hasReadGenericPermission(Resource.ALARM)
   ],
   [
+    MenuId.alarm_rules, (_authState, userPermissionsService) =>
+    userPermissionsService.hasResourcesGenericPermission([Resource.DEVICE, Resource.ASSET, Resource.CUSTOMER, Resource.DEVICE_PROFILE, Resource.ASSET_PROFILE],Operation.READ_CALCULATED_FIELD)
+  ],
+  [
     MenuId.dashboard_all, (_authState, userPermissionsService) =>
           userPermissionsService.hasReadGenericPermission(Resource.DASHBOARD)
   ],
