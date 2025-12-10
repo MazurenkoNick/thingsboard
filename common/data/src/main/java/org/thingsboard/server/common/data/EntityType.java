183555dbbd35f7e2e2999b1c975d06b8b60fb8e2
dashevchenko <dshevchenko@thingsboard.io>
2025-12-10 19:40:37 +0200

    fixed ordinal order for EntityType enum

--- common/data/src/main/java/org/thingsboard/server/common/data/EntityType.java
+++ common/data/src/main/java/org/thingsboard/server/common/data/EntityType.java
@@ -59,8 +59,6 @@ public enum EntityType {
     RULE_NODE(12),
     SCHEDULER_EVENT(103),
     BLOB_ENTITY(104),
-    REPORT_TEMPLATE(108),
-    REPORT(109),
     ENTITY_VIEW(15, true) {
         // backward compatibility for TbOriginatorTypeSwitchNode to return correct rule node connection.
         @Override
@@ -101,7 +99,9 @@ public enum EntityType {
         public String getNormalName() {
             return "AI model";
         }
-    };
+    },
+    REPORT_TEMPLATE(108),
+    REPORT(109);
 
     @Getter
     private final int protoNumber; // Corresponds to EntityTypeProto
