bcaecc963c486df4dadae192fe66f0f02dbe5b9f
Andrii Landiak <andriylandiak@gmail.com>
2025-12-29 13:09:42 +0200

    Add license for link

--- application/src/main/java/org/thingsboard/server/controller/NotificationController.java
+++ application/src/main/java/org/thingsboard/server/controller/NotificationController.java
@@ -446,9 +446,11 @@ public class NotificationController extends BaseController {
                 NotificationTargetId notificationTargetId = sysAdmins.get().getId();
                 String baseUrl = systemSecurityService.getBaseUrl(TenantId.SYS_TENANT_ID, new CustomerId(EntityId.NULL_UUID), request);
                 String checkConfigurationActionLabel = "Check configuration";
-                String checkConfigurationLink = null;
+                String checkConfigurationLink;
                 if (addonType == AddonType.TRENDZ && subscriptionService.getLicenseVersion() < 2) {
                     checkConfigurationLink = "/trendzSettings";
+                } else {
+                    checkConfigurationLink = "/license";
                 }
 
                 NotificationInfo info = AddonAccessErrorNotificationInfo.builder()
