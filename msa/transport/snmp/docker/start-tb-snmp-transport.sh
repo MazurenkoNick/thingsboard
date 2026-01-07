2a7749e2d25d0c047030f152c8cc4a670233452a
Viacheslav Klimov <viacheslavklimov11@gmail.com>
2026-01-07 14:59:08 +0200

    Update license header

--- msa/transport/snmp/docker/start-tb-snmp-transport.sh
+++ msa/transport/snmp/docker/start-tb-snmp-transport.sh
@@ -2,7 +2,7 @@
 #
 # The Thingsboard Authors ("COMPANY") CONFIDENTIAL
 #
-# Copyright © 2016-2021 The Thingsboard Authors All Rights Reserved.
+# Copyright © 2016-2026 The Thingsboard Authors All Rights Reserved.
 #
 # NOTICE: All information contained herein is, and remains
 # the property of The Thingsboard Authors and its suppliers,
