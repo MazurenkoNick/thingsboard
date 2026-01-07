24b71df2f7f881677bff6a38cad56beabeea5ee1
Maksym Tsymbarov <mtsymbarov@thingsboard.io>
2026-01-07 16:47:08 +0200

    Enhanced title determination for entity alias and filter in reports

--- ui-ngx/src/app/modules/home/components/alias/entity-aliases-dialog.component.ts
+++ ui-ngx/src/app/modules/home/components/alias/entity-aliases-dialog.component.ts
@@ -61,7 +61,9 @@ import {
   ReportComponentConfig,
   ReportComponentType
 } from '@shared/models/report-component.models';
-import { reportComponentTypesData } from '@home/pages/reporting/template/components/report-component.models';
+import {
+  reportComponentTypesData
+} from '@home/pages/reporting/template/components/report-component.models';
 
 export interface EntityAliasesDialogData {
   entityAliases: EntityAliases;
@@ -151,17 +153,19 @@ export class EntityAliasesDialogComponent extends DialogComponent<EntityAliasesD
 
     if(data.reportMode && data.reportComponents.length) {
       this.data.reportComponents.forEach((component) => {
-        const typedComponent = component as DataReportComponentConfig;
-        if (typedComponent.type === ReportComponentType.ALARM_TABLE) {
-          const alarmSource = (typedComponent as AlarmTableReportComponentConfig).alarmSource
+        if (component.type === ReportComponentType.ALARM_TABLE) {
+          const alarmSource = (component as AlarmTableReportComponentConfig).alarmSource
           if (alarmSource) {
-            this.addWidgetTitleToWidgetsMap(alarmSource.entityAliasId, typedComponent.type);
+            this.addWidgetTitleToWidgetsMap(alarmSource.entityAliasId,
+              reportComponentTypesData.getReportComponentTypeData(component.type, component.subType).title);
           }
         } else {
-          typedComponent.dataSources.forEach((datasource) => {
+          const dataSources = (component as DataReportComponentConfig).dataSources;
+          dataSources.forEach((datasource) => {
             if ([DatasourceType.entity, DatasourceType.entityCount, DatasourceType.alarmCount].includes(datasource.type)
               && datasource.entityAliasId) {
-              this.addWidgetTitleToWidgetsMap(datasource.entityAliasId, typedComponent.type);
+              this.addWidgetTitleToWidgetsMap(datasource.entityAliasId,
+                reportComponentTypesData.getReportComponentTypeData(component.type, component.subType).title);
             }
           });
         }
@@ -229,10 +233,7 @@ export class EntityAliasesDialogComponent extends DialogComponent<EntityAliasesD
     if (widgetsTitleList) {
       let widgetsListHtml = '';
       for (const widgetTitle of widgetsTitleList) {
-        const title = this.data.reportMode ?
-          this.translate.instant(reportComponentTypesData.getReportComponentTypeData(widgetTitle as ReportComponentType).title)
-          : widgetTitle;
-        widgetsListHtml += '<br/>\'' + title + '\'';
+        widgetsListHtml += '<br/>\'' + this.translate.instant(widgetTitle) + '\'';
       }
       const messageKey = this.data.reportMode ? 'entity.unable-delete-entity-alias-text-components' : 'entity.unable-delete-entity-alias-text';
       const message = this.translate.instant(messageKey,
--- ui-ngx/src/app/modules/home/components/filter/filters-dialog.component.ts
+++ ui-ngx/src/app/modules/home/components/filter/filters-dialog.component.ts
@@ -142,8 +142,7 @@ export class FiltersDialogComponent extends DialogComponent<FiltersDialogCompone
     if(data.reportMode && data.reportComponents.length) {
       let componentsTitleList: Array<string>;
       this.data.reportComponents.forEach((component) => {
-        const typedComponent = component as DataReportComponentConfig;
-        const datasources = typedComponent.type === ReportComponentType.ALARM_TABLE ? [(typedComponent as AlarmTableReportComponentConfig).alarmSource] : typedComponent.dataSources;
+        const datasources = component.type === ReportComponentType.ALARM_TABLE ? [(component as AlarmTableReportComponentConfig).alarmSource] : (component as DataReportComponentConfig).dataSources;
         datasources.forEach((datasource) => {
           if (datasource.filterId) {
             componentsTitleList = this.filterToWidgetsMap[datasource.filterId];
@@ -151,8 +150,8 @@ export class FiltersDialogComponent extends DialogComponent<FiltersDialogCompone
               componentsTitleList = [];
               this.filterToWidgetsMap[datasource.filterId] = componentsTitleList;
             }
-            if (!componentsTitleList.includes(typedComponent.type)) {
-              componentsTitleList.push(typedComponent.type);
+            if (!componentsTitleList.includes(reportComponentTypesData.getReportComponentTypeData(component.type, component.subType).title)) {
+              componentsTitleList.push(reportComponentTypesData.getReportComponentTypeData(component.type, component.subType).title);
             }
           }
         });
@@ -203,10 +202,7 @@ export class FiltersDialogComponent extends DialogComponent<FiltersDialogCompone
     if (widgetsTitleList) {
       let widgetsListHtml = '';
       for (const widgetTitle of widgetsTitleList) {
-        const title = this.data.reportMode ?
-          this.translate.instant(reportComponentTypesData.getReportComponentTypeData(widgetTitle as ReportComponentType).title)
-          : widgetTitle;
-        widgetsListHtml += '<br/>\'' + title + '\'';
+        widgetsListHtml += '<br/>\'' + this.translate.instant(widgetTitle) + '\'';
       }
       const messageKey = this.data.reportMode ? 'filter.unable-delete-filter-text-components' : 'entity.unable-delete-filter-text';
       const message = this.translate.instant(messageKey,
