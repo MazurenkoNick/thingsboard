abe6480a599da4d9708b50fbbd934eae1a45810d
Viacheslav Klimov <viacheslavklimov11@gmail.com>
2026-01-29 15:34:32 +0200

    Get back upgrade from 4.2.0 to 4.2.1

--- application/src/main/data/upgrade/basic/schema_update.sql
+++ application/src/main/data/upgrade/basic/schema_update.sql
@@ -29,4 +29,72 @@
 -- OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
 --
 
--- FIXME GET FROM v4.2 tag
+-- UPDATE OTA PACKAGE EXTERNAL ID START
+
+ALTER TABLE ota_package
+    ADD COLUMN IF NOT EXISTS external_id uuid;
+
+DO
+$$
+    BEGIN
+        IF NOT EXISTS(SELECT 1 FROM pg_constraint WHERE conname = 'ota_package_external_id_unq_key') THEN
+            ALTER TABLE ota_package ADD CONSTRAINT ota_package_external_id_unq_key UNIQUE (tenant_id, external_id);
+        END IF;
+    END;
+$$;
+
+-- UPDATE OTA PACKAGE EXTERNAL ID END
+
+-- UPDATE SCHEDULER_EVENT EXTERNAL ID START
+
+ALTER TABLE scheduler_event
+    ADD COLUMN IF NOT EXISTS external_id uuid;
+
+DO
+$$
+    BEGIN
+        IF NOT EXISTS(SELECT 1 FROM pg_constraint WHERE conname = 'scheduler_event_external_id_unq_key') THEN
+            ALTER TABLE scheduler_event ADD CONSTRAINT scheduler_event_external_id_unq_key UNIQUE (tenant_id, external_id);
+        END IF;
+    END;
+$$;
+
+-- UPDATE SCHEDULER_EVENT EXTERNAL ID END
+
+-- UPDATE NEW REPORT FEATURE START
+
+UPDATE scheduler_event SET type = 'generateDashboardReport' WHERE type = 'generateReport';
+ALTER TABLE api_usage_state ADD COLUMN IF NOT EXISTS report_exec varchar(32) DEFAULT 'ENABLED';
+
+-- UPDATE NEW REPORT FEATURE END
+
+-- DROP INDEXES THAT DUPLICATE UNIQUE CONSTRAINT START
+
+DROP INDEX IF EXISTS idx_device_external_id;
+DROP INDEX IF EXISTS idx_device_profile_external_id;
+DROP INDEX IF EXISTS idx_asset_external_id;
+DROP INDEX IF EXISTS idx_entity_view_external_id;
+DROP INDEX IF EXISTS idx_rule_chain_external_id;
+DROP INDEX IF EXISTS idx_dashboard_external_id;
+DROP INDEX IF EXISTS idx_customer_external_id;
+DROP INDEX IF EXISTS idx_widgets_bundle_external_id;
+-- PE
+DROP INDEX IF EXISTS idx_converter_external_id;
+DROP INDEX IF EXISTS idx_integration_external_id;
+DROP INDEX IF EXISTS idx_role_external_id;
+
+-- DROP INDEXES THAT DUPLICATE UNIQUE CONSTRAINT END
+
+ALTER TABLE mobile_app ADD COLUMN IF NOT EXISTS title varchar(255);
+
+DELETE FROM integration where type = 'IBM_WATSON_IOT';
+
+DELETE FROM converter where integration_type = 'IBM_WATSON_IOT';
+
+-- UPDATE EDGE LICENSE KEY TO SUPPORT OFFLINE FEATURE START
+
+DROP VIEW IF EXISTS edge_info_view CASCADE;
+DROP VIEW IF EXISTS edge_active_attribute_view CASCADE;
+ALTER TABLE edge ALTER COLUMN edge_license_key TYPE varchar;
+
+-- UPDATE EDGE LICENSE KEY TO SUPPORT OFFLINE FEATURE END
