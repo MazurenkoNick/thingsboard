e84a05ee692425f39d9b14295783d3baedb76160
Viacheslav Klimov <viacheslavklimov11@gmail.com>
2026-01-29 12:45:04 +0200

    Revert "Cleanup upgrade to 4.3 for 4.3.0.1"
    This reverts commit 386ac9526b205102c6866a686ea8639827c4d84e.

--- dao/src/main/java/org/thingsboard/server/dao/sql/widget/JpaWidgetTypeDao.java
+++ dao/src/main/java/org/thingsboard/server/dao/sql/widget/JpaWidgetTypeDao.java
@@ -58,6 +58,7 @@ import org.thingsboard.server.dao.sql.JpaAbstractDao;
 import org.thingsboard.server.dao.util.SqlDao;
 import org.thingsboard.server.dao.widget.WidgetTypeDao;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -306,4 +307,11 @@ public class JpaWidgetTypeDao extends JpaAbstractDao<WidgetTypeDetailsEntity, Wi
         widgetTypeInfoRepository.labelWidgetTypesAsDeprecatedByFqns(fqns);
     }
 
+    @Override
+    public Set<String> findUniqueExternalHostsInAnalyticsBundleByFqns(Collection<String> fqns) {
+        if (fqns.isEmpty()) {
+            return Collections.emptySet();
+        }
+        return widgetTypeInfoRepository.findUniqueExternalHostsInAnalyticsBundleByFqns(fqns);
+    }
 }
