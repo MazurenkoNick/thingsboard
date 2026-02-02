e0fb85834153b223e691289f613f1079c67ec2a4
Viacheslav Klimov <viacheslavklimov11@gmail.com>
2026-02-02 12:17:09 +0200

    Version set to 4.3.0.1PE

--- release-upload.sh
+++ release-upload.sh
@@ -34,7 +34,7 @@ set -euo pipefail
 
 # === Configuration ===
 TMP_DIR="/tmp/tb-release"
-RELEASE="4.3pe"
+RELEASE="4.3.0.1pe"
 S3_BUCKET="s3://cf-simple-s3-origin-tb-pe-cdn-156597721064/"
 
 # Clean & recreate temp dir
