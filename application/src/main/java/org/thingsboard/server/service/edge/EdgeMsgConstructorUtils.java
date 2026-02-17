0065218f179baf545ce80ab7f852b9e69591ee99
Volodymyr Babak <volodymyr.babak@gmail.com>
2026-02-17 12:55:42 +0200

    Support older versions

--- application/src/main/java/org/thingsboard/server/service/edge/EdgeMsgConstructorUtils.java
+++ application/src/main/java/org/thingsboard/server/service/edge/EdgeMsgConstructorUtils.java
@@ -409,14 +409,10 @@ public class EdgeMsgConstructorUtils {
                 .setType(edge.getType())
                 .setRoutingKey(edge.getRoutingKey())
                 .setSecret(edge.getSecret())
+                .setEdgeLicenseKey(edge.getEdgeLicenseKey() != null ? edge.getEdgeLicenseKey() : "")
+                .setCloudEndpoint(edge.getCloudEndpoint() != null ? edge.getCloudEndpoint() : "")
                 .setAdditionalInfo(JacksonUtil.toString(edge.getAdditionalInfo()))
                 .setCloudType("PE");
-        if (edge.getEdgeLicenseKey() != null) {
-            builder.setEdgeLicenseKey(edge.getEdgeLicenseKey());
-        }
-        if (edge.getCloudEndpoint() != null) {
-            builder.setCloudEndpoint(edge.getCloudEndpoint());
-        }
         if (edge.getCustomerId() != null) {
             builder.setCustomerIdMSB(edge.getCustomerId().getId().getMostSignificantBits())
                     .setCustomerIdLSB(edge.getCustomerId().getId().getLeastSignificantBits());
