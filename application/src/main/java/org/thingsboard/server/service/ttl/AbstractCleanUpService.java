e70b517c4dcf970dac49243eeb1296acdd0d2559
Andrii Landiak <andriylandiak@gmail.com>
2026-03-06 13:33:19 +0200

    PE: Fixed notification requests and RPC cleanup timeout on large datasets

--- application/src/main/java/org/thingsboard/server/service/ttl/AbstractCleanUpService.java
+++ application/src/main/java/org/thingsboard/server/service/ttl/AbstractCleanUpService.java
@@ -47,4 +47,8 @@ public abstract class AbstractCleanUpService {
         return partitionService.resolve(ServiceType.TB_CORE, TenantId.SYS_TENANT_ID, TenantId.SYS_TENANT_ID).isMyPartition();
     }
 
+    protected boolean isTenantPartitionMine(TenantId tenantId) {
+        return partitionService.resolve(ServiceType.TB_CORE, tenantId, tenantId).isMyPartition();
+    }
+
 }
